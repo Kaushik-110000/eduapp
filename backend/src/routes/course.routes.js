0b0166ad24cd9fb6e190a1cd0952105ab6cc594c
Kaushik-110000 <abhishekkumar89647@gmail.com>
2025-04-27 11:04:47 +0530

    ahah

--- backend/src/routes/course.routes.js
+++ backend/src/routes/course.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { allCourses, getCourse } from "../controllers/course.controller.js";
+import {
+  allCourses,
+  getCourse,
+  getAllVideos,
+} from "../controllers/course.controller.js";
 import { createRoom } from "../controllers/video.controller.js";
 const router = Router();
 import { verifyTutor } from "../middlewares/auth.middleware.js";
 router.route("/all").get(allCourses);
 router.route("/createVideo").post(verifyTutor, createRoom);
 router.route("/:courseId").get(getCourse);
+router.route("/videos/:courseId").get(getAllVideos);
 export default router;
